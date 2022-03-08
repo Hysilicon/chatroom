e4ed3f01e93d7ab13da769155234b829c0c1e0ff
hysilicon <hisilicon@163.com>
2022-03-08 14:01:50 +0800

    Update on 3.7.2022

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,22 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+//Get username and room
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const socket = io();
+
+// Join chatroom
+socket.emit("joinroom", { username, room });
+
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomNameToDOM(room);
+  outputUsersToDOM(users);
+});
 
 socket.on("message", (message) => {
   console.log(message);
@@ -28,10 +43,32 @@ const outputMessage = (message) => {
   // Create a new div and append it to the html
   const newDiv = document.createElement("div");
   newDiv.classList.add("message");
-  newDiv.innerHTML = `<p class="meta">Brad <span>9:12 pm</span></p>
+  newDiv.innerHTML = `<p class="meta">${message.username} <span> ${message.time}</span></p>
     <p class="text">
-      ${message}
+      ${message.text}
     </p>`;
 
   document.querySelector(".chat-messages").appendChild(newDiv);
 };
+
+const outputRoomNameToDOM = (room) => {
+  roomName.innerText = room;
+};
+
+const outputUsersToDOM = (users) => {
+  userList.innerHTML = "";
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+};
+
+//Prompt the user before leave chat room
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
+  if (leaveRoom) {
+    window.location = "../index.html";
+  } else {
+  }
+});
